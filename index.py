ac411dcc528cd8cc4b72e8f1c51fa4b4e763eedd
Ali Baker <alisondre.baker@gmail.com>
2026-06-21 14:11:04 -0400

    Added while loop

--- index.py
+++ index.py
@@ -29,3 +29,29 @@ user_menu_list = ["Add task", "View tasks", "Delete tasks", "Quit application"]
 user_tasks = []
 
 greet_user("Ali")
+
+while True:
+    user_choice = show_user_menu(user_menu_list)
+    
+    if user_choice == "1":
+        add_task(user_tasks)
+        print(f"Current tasks: {user_tasks}\n")
+    
+    elif user_choice == "2":
+        print("\n---YOUR TASKS ---")
+        if not user_tasks:
+            print("Your list is empty!")
+        else:
+            for i, task in enumerate(user_tasks, 1):
+                print(f"{i}. {task}")
+        print()
+    
+    elif user_choice == "3":
+        print("\nDelete functionality coming up next!")
+        
+    elif user_choice == "4":
+        print("\nthank you for using To-Do App! Goodbye.")
+        break
+    
+    else:
+        print("\n⚠️ Invalid selection. Please type 1, 2, 3, or 4.\n")
