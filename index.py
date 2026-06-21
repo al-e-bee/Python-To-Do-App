8f6c196bf11535848b5eb2c9301df8b4fa078d14
Ali Baker <alisondre.baker@gmail.com>
2026-06-21 13:22:24 -0400

    First commit

---
+++ index.py
@@ -0,0 +1,8 @@
+def greet_user(name):
+    print(f"Welcome {name}, here is your to-do list!")
+
+print(greet_user("Ali"))
+
+user_menu = ["Add task", "View tasks", "Delete tasks", "Quit application"]
+
+
